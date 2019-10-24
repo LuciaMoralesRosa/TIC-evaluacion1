9946b4d9ba53efaabf2a016f8ad41c39152eeb19
Lucía <55547174+LuciaMoralesRosa@users.noreply.github.com>
2019-10-24 16:26:29 +0200

    Add files via upload

---
+++ Ejercicio 3.py
@@ -0,0 +1,6 @@
+def dia():
+    lista = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+    x = input ("Introduzca un numero del 1 al 7: ")
+    print (lista[x-1])
+
+dia()
