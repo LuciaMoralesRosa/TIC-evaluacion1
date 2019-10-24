34e42da7a330ab468e58ffdcfb26b13d0d2afbd7
Lucía <55547174+LuciaMoralesRosa@users.noreply.github.com>
2019-10-24 16:33:54 +0200

    Add files via upload

---
+++ Ejercicio 4.py
@@ -0,0 +1,6 @@
+def mes():
+    lista = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    x = input ("Introduzca un numero del 1 al 12: ")
+    print "El mes al que corresponde ese numero es", (lista[x-1])
+
+mes()
