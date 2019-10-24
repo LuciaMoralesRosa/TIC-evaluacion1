7a0dbfc9b773ce18707c0cdff7fe888f1e4710a8
Lucía <55547174+LuciaMoralesRosa@users.noreply.github.com>
2019-10-24 17:06:16 +0200

    Add files via upload

---
+++ Ejercicio 5.py
@@ -0,0 +1,7 @@
+def fecha():
+    a, b, c = input ("inserte ...: ")
+    listab = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+    print a,"de", (listab [b-1]),"de", c
+     
+fecha()
