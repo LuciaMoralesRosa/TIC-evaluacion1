decdcd7551f4d82073e4985788542238899866e7
Lucía <55547174+LuciaMoralesRosa@users.noreply.github.com>
2019-11-01 11:19:12 +0100

    Delete Ejercicio 6 TERMINAR.py

--- Ejercicio 6 TERMINAR.py
+++
@@ -1,9 +0,0 @@
-def ejercicio_6():
-    x = input ("introduzca un numero: ")
-    y = input ("introduzca otro numero: ")
-    suma = +
-    z = raw_input ("que operacion quiere realizar? ")
-    resultado = x z y
-    print resultado
-
-ejercicio_6()
