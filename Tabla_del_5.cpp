4aee964811a7b1888d19e9c37d1255ab9176a7f0
Lucía <55547174+LuciaMoralesRosa@users.noreply.github.com>
2019-11-28 09:47:48 +0100

    Add files via upload

---
+++ Tabla_del_5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main (){
+    //Definicion de variables
+    int ntabla;
+    char salir;
+    std::cout <<"Que tabla deseas que muestre: ";
+    std::cin >> ntabla;
+    std::cout <<"*****************************";
+    std::cout <<"TABLA DEL ", ntabla,"      *";
+    std::cin >> salir;
+
+}
+    
